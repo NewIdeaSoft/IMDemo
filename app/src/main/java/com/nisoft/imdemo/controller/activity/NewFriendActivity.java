3d682656f985e52c6eb2f191b788d7cc127986ae
Yangtao <yangtao9651@163.com>
2017-09-17 07:49:42 +0800

    查找添加新朋友

---
+++ app/src/main/java/com/nisoft/imdemo/controller/activity/NewFriendActivity.java
@@ -0,0 +1,104 @@
+package com.nisoft.imdemo.controller.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.exceptions.HyphenateException;
+import com.nisoft.imdemo.R;
+import com.nisoft.imdemo.module.Module;
+import com.nisoft.imdemo.module.bean.UserInfo;
+
+public class NewFriendActivity extends Activity {
+
+    private TextView tv_new_friend_search;
+    private EditText et_new_friend_name;
+    private LinearLayout ll_new_friend_search_result;
+    private ImageView iv_new_friend_search_result_icon;
+    private TextView tv_new_friend_search_result_name;
+    private Button btn_new_friend_add;
+    private UserInfo mNewFriendInfo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_friend);
+        initView();
+        initListener();
+    }
+
+    private void initListener() {
+        tv_new_friend_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findFriend();
+            }
+        });
+        btn_new_friend_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNewFriend();
+            }
+        });
+    }
+
+    private void findFriend() {
+        final String friendName = et_new_friend_name.getText().toString();
+
+        UserInfo userInfo = new UserInfo();
+        userInfo.setName(friendName);
+        mNewFriendInfo = userInfo;
+
+        tv_new_friend_search_result_name.setText(mNewFriendInfo.getName());
+//        Module.getInstance().getGlobalThreadPool().execute(new Runnable() {
+//            @Override
+//            public void run() {
+//                final String friendName = et_new_friend_name.getText().toString();
+//
+//                UserInfo userInfo = new UserInfo();
+//                userInfo.setName(friendName);
+//
+//            }
+//        });
+    }
+
+    private void addNewFriend() {
+        Module.getInstance().getGlobalThreadPool().execute(new Runnable() {
+            String toastText;
+
+            @Override
+            public void run() {
+                try {
+                    EMClient.getInstance().contactManager().addContact(mNewFriendInfo.getName(), "添加好友");
+                    toastText = "添加成功！";
+                } catch (HyphenateException e) {
+                    e.printStackTrace();
+                    toastText = "添加失败！错误：" + e.toString();
+                }finally {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(NewFriendActivity.this, toastText, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void initView() {
+        tv_new_friend_search = (TextView) findViewById(R.id.tv_new_friend_search);
+        et_new_friend_name = (EditText) findViewById(R.id.et_new_friend_name);
+        ll_new_friend_search_result = (LinearLayout) findViewById(R.id.ll_new_friend_search_result);
+        iv_new_friend_search_result_icon = (ImageView) findViewById(R.id.iv_new_friend_search_result_icon);
+        tv_new_friend_search_result_name = (TextView) findViewById(R.id.tv_new_friend_search_result_name);
+        btn_new_friend_add = (Button) findViewById(R.id.btn_new_friend_add);
+    }
+}
--- app/src/main/java/com/nisoft/imdemo/controller/fragment/ContactsFragment.java
+++ app/src/main/java/com/nisoft/imdemo/controller/fragment/ContactsFragment.java
@@ -1,18 +1,43 @@
 package com.nisoft.imdemo.controller.fragment;
 
+import android.content.Intent;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.hyphenate.easeui.ui.EaseContactListFragment;
 import com.nisoft.imdemo.R;
+import com.nisoft.imdemo.controller.activity.NewFriendActivity;
 
 /**
  * Created by Administrator on 2017/9/15.
  */
 
 public class ContactsFragment extends EaseContactListFragment {
+    private LinearLayout ll_fragment_main_contacts_new_friend;
+    private LinearLayout ll_fragment_main_contacts_group;
     @Override
     protected void initView() {
         super.initView();
-        View.inflate(getActivity(), R.layout.header_fragment_main_contacts,null);
+        titleBar.setTitle("联系人");
+        View view = View.inflate(getActivity(), R.layout.header_fragment_main_contacts, null);
+        ll_fragment_main_contacts_new_friend =
+                (LinearLayout) view.findViewById(R.id.ll_fragment_main_contacts_new_friend);
+        ll_fragment_main_contacts_group =
+                (LinearLayout) view.findViewById(R.id.ll_fragment_main_contacts_group);
+        ll_fragment_main_contacts_new_friend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),NewFriendActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ll_fragment_main_contacts_group.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        listView.addHeaderView(view);
     }
 }
