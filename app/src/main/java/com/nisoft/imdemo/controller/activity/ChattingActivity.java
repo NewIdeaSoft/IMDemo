e88de1cf1e1329060757393beac53f863130da3a
YangTao <yangtao9651@163.com>
2017-10-19 18:16:29 +0800

    会话列表

--- app/src/main/java/com/nisoft/imdemo/controller/activity/ChattingActivity.java
+++ app/src/main/java/com/nisoft/imdemo/controller/activity/ChattingActivity.java
@@ -16,9 +16,13 @@ public class ChattingActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chating);
         String hxid = getIntent().getStringExtra("hxid");
+        String chatType = getIntent().getStringExtra(EaseConstant.EXTRA_CHAT_TYPE);
         Fragment fragment = new EaseChatFragment();
         Bundle args = new Bundle();
         args.putString(EaseConstant.EXTRA_USER_ID,hxid);
+        if(chatType!=null) {
+            args.putString(EaseConstant.EXTRA_CHAT_TYPE,chatType);
+        }
         fragment.setArguments(args);
         FragmentManager fm = getSupportFragmentManager();
         fm.beginTransaction().add(R.id.chatting_fragment_content,fragment).commit();
--- app/src/main/java/com/nisoft/imdemo/controller/fragment/ChatFragment.java
+++ app/src/main/java/com/nisoft/imdemo/controller/fragment/ChatFragment.java
@@ -1,5 +1,6 @@
 package com.nisoft.imdemo.controller.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,24 +8,66 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.hyphenate.EMMessageListener;
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMConversation;
+import com.hyphenate.chat.EMMessage;
+import com.hyphenate.easeui.EaseConstant;
+import com.hyphenate.easeui.EaseUI;
+import com.hyphenate.easeui.ui.EaseConversationListFragment;
+import com.nisoft.imdemo.controller.activity.ChattingActivity;
+
+import java.util.List;
+
 /**
  * Created by Administrator on 2017/9/15.
  */
 
-public class ChatFragment extends Fragment {
+public class ChatFragment extends EaseConversationListFragment {
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    protected void initView() {
+        super.initView();
+        setConversationListItemClickListener(new EaseConversationListItemClickListener() {
+            @Override
+            public void onListItemClicked(EMConversation conversation) {
+                Intent intent = new Intent(getActivity(), ChattingActivity.class);
+                intent.putExtra("hxid",conversation.conversationId());
+                if(conversation.getType()== EMConversation.EMConversationType.GroupChat) {
+                    intent.putExtra(EaseConstant.EXTRA_CHAT_TYPE,EaseConstant.CHATTYPE_GROUP);
+                }
+                startActivity(intent);
+            }
+        });
+        EMClient.getInstance().chatManager().addMessageListener(new EMMessageListener() {
+            @Override
+            public void onMessageReceived(List<EMMessage> list) {
+                EaseUI.getInstance().getNotifier().onNewMesg(list);
+            }
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
-    }
+            @Override
+            public void onCmdMessageReceived(List<EMMessage> list) {
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
+            }
+
+            @Override
+            public void onMessageRead(List<EMMessage> list) {
+
+            }
+
+            @Override
+            public void onMessageDelivered(List<EMMessage> list) {
+
+            }
+
+            @Override
+            public void onMessageRecalled(List<EMMessage> list) {
+
+            }
+
+            @Override
+            public void onMessageChanged(EMMessage emMessage, Object o) {
+
+            }
+        });
     }
 }
